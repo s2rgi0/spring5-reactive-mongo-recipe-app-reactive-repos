8ee4fde6ac9dd54b494a3ca0a177c4f5aaba2ec9
s2rgi0 <tlakolu@gmail.com>
2022-08-17 18:44:13 -0600

    added reactive Ingrediente service delete test

--- src/test/java/guru/springframework/controllers/IngredientControllerTest.java
+++ src/test/java/guru/springframework/controllers/IngredientControllerTest.java
@@ -140,6 +140,7 @@ public class IngredientControllerTest {
     @Test
     public void testDeleteIngredient() throws Exception {
 
+        when(ingredientService.deleteById(anyString(), anyString())).thenReturn(Mono.empty());
         //then
         mockMvc.perform(get("/recipe/2/ingredient/3/delete")
         )
